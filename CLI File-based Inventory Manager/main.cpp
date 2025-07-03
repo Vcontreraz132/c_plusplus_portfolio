1c7ccf6c2f020dc13d424658afe33d59dde3d92d
VincentContreraz <vcontreraz132@gmail.com>
2025-07-03 15:07:46 -0700

    Initial commit of CLI-based inventory manager

---
+++ CLI File-based Inventory Manager/main.cpp
@@ -0,0 +1,19 @@
+/*2. File-Based Inventory Manager
+Goals:
+
+Practice file I/O (<fstream>) and basic data persistence
+
+Start using STL containers (std::vector, std::map)
+
+Features to implement:
+
+Load/save “items” (ID, name, quantity, price) to a CSV or JSON-style text file
+
+Add/remove/update items via a menu
+
+Simple search/filter by name or price range*/
+
+
+#include <iostream>
+#include <vector>
+#include <string>

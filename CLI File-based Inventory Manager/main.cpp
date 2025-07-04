9c543d7dca5a029436639e4b97aba86c8ef3da15
root <root@DESKTOP-T67KNAC.localdomain>
2025-07-04 11:49:09 -0700

    implemented startup routine that checks for save directory and files then creates them if do not exist

--- CLI File-based Inventory Manager/main.cpp
+++ CLI File-based Inventory Manager/main.cpp
@@ -17,3 +17,56 @@ Simple search/filter by name or price range*/
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <filesystem>
+
+#include "functions.hpp"
+
+
+int main() {
+    // prompt user to load file
+    std::cout << "Welcome" << std::endl;
+    // scan save directory if it exists
+    std::string save_directory = "saves";
+    std::string target_extension = ".csv";
+
+    namespace fs = std::filesystem;
+
+    try {
+
+        if(fs::exists(save_directory) && fs::is_directory(save_directory)) {
+            std::cout << "Please choose a file to load" << std::endl;
+
+            for(const auto& entry : fs::directory_iterator(save_directory)) {
+                std::cout << "scanning save directory" << std::endl;
+
+                if(fs::is_regular_file(entry.status())) {
+                    std::cout << ""
+
+                    if(entry.path().extension() == target_extension) {
+
+                        std::cout << "vaild save file found" << std::endl;
+                        std::cout << entry.path().filename() << std::endl;
+                    } else { // no valid .csv files found
+                        std::cout << "Directory found; no valid files" << std::endl;
+                        createSave(save_directory);
+                    }
+                }
+            }
+        } else { // no save directory found
+            
+            std::cerr << "No save directory found" << std::endl;
+            // create new save directory
+            if(std::filesystem::create_directories(save_directory)) {
+
+                std::cout << "directory created\n";
+                createSave(save_directory); // create save file
+            }
+        }
+    } catch (const fs::filesystem_error& ex) {
+
+        std::cerr << "Filesystem error: " << ex.what() << std::endl;
+    }
+
+    return 0;
+}

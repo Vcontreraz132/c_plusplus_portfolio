35851c07cf0c6357136a38bad6941ac8eef87fcf
VincentContreraz <vcontreraz132@gmail.com>
2025-07-08 10:25:53 -0700

    added function declarations

--- CLI File-based Inventory Manager/functions.hpp
+++ CLI File-based Inventory Manager/functions.hpp
@@ -1,3 +1,5 @@
 int createSave(std::string save_directory);
 
 int scanSaveDirectory(std::string save_directory, std::string target_extension);
+
+int loadSave(std::string save_directory, std::string save_file_name);
